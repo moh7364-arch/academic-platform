b410b3e575ed3632acc80e454615ef57a44a7c74
moh7364-arch <kaleemjamee7364@gmail.com>
2026-04-26 05:08:52 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,6 @@
-// Firebase SDK (نسخة تعمل مباشرة في المتصفح)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+export const ADMIN_EMAIL = "kaleemjamee7364@gmail.com";
 
-// إعدادات Firebase الخاصة بك
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyDqsm-zt9r6zjKXgSwmhJk60No5cA_mUvk",
   authDomain: "academic-platform-a499e.firebaseapp.com",
   projectId: "academic-platform-a499e",
@@ -14,11 +9,3 @@ const firebaseConfig = {
   appId: "1:99095620829:web:05455e7ffa610692ed3f34",
   measurementId: "G-PX6ML2W3XZ"
 };
-
-// تشغيل Firebase
-const app = initializeApp(firebaseConfig);
-
-// الخدمات
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
